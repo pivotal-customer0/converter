09d9fa0759e0f255d34e15ad18b1998a4227e9be
krujos <jkruck@pivotal.io>
2015-11-12 12:45:21 -0600

    [#107928712 fixes test]

--- lib/converter/cf_portgroup.rb
+++ lib/converter/cf_portgroup.rb
@@ -3,7 +3,7 @@ require 'converter/yaml_helper'
 #NOTE This class only supports one network.
 
 class CFPortGroup
-  def self.set_portgrp(yaml, new_portgrp)
-    yaml['networks'][0]['subnets'][0]['cloud_properties']['name'] = new_portgrp
+  def self.set_portgroup(yaml, new_portgroup)
+    yaml['networks'][0]['subnets'][0]['cloud_properties']['name'] = new_portgroup
   end
 end
--- spec/cf_portgroup_spec.rb
+++ spec/cf_portgroup_spec.rb
@@ -4,59 +4,29 @@ require 'converter/cf_portgroup'
 describe CFPortGroup do
   describe 'vSphere/Photon PortGroup manipulation' do
     yaml = nil
+
     context 'in the cf manifest' do
-     manifest = <<-END
+      manifest = <<-END
+
 ---
-name: cf-3b6ffad4633b52c03f3e
-director_uuid: ignore
-releases:
-- name: cf-mysql
-  version: '23'
-- name: cf
-  version: '222'
-- name: diego
-  version: 0.1437.0
-- name: garden-linux
-  version: 0.308.0
-- name: etcd
-  version: '16'
-- name: push-apps-manager-release
-  version: '397'
-- name: notifications
-  version: '19'
-- name: notifications-ui
-  version: '10'
-- name: cf-autoscaling
-  version: '28'
 networks:
 - name: default
   subnets:
-  - range: 192.168.200.0/24
-    gateway: 192.168.200.1
-    dns:
-    - 192.168.10.2
-    static:
-    - 192.168.200.11
-    - 192.168.200.12
-    - 192.168.200.13
-    reserved:
-    - 192.168.200.2-192.168.200.10
-    - 192.168.200.79-192.168.200.113
-    - 192.168.200.115-192.168.200.254
+  - range: 10.65.170.0/23
     cloud_properties:
-      name: vxw-dvs-38-virtualwire-1-sid-5000-PCF Deployment Network
-resource_pools:
+      name: Photon_PCF
 
 END
 
- before do
-     yaml = YAML.load manifest
- end
- 
- it 'should get the current network' do
-        expect(yaml['networks'][0]['subnets'][0]['cloud_properties']['name']).to eql 'vxw-dvs-38-virtualwire-1-sid-5000-PCF Deployment Network'
-      end
-      
+     before do
+       yaml = YAML.load manifest
+     end
+
+     it 'should swap the network' do
+        CFPortGroup.set_portgroup yaml, 'a-new-network'
+        expect(yaml['networks'][0]['subnets'][0]['cloud_properties']['name']).to eql 'a-new-network'
+     end
+
   end
  end
 end
