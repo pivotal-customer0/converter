49df7510cc1943dac76825986ecba3df2ee4a709
krujos <jkruck@pivotal.io>
2016-02-04 12:09:19 -0600

    Add stemcell versions

--- bin/redis.sh
+++ bin/redis.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash -x
 bundle exec converter --name redis --swap-ip-ranges 10.65.170:10.65.172,10.65.171:10.65.173 \
     --domain cf.haas-04.pez.pivotal.io --sanitize-partition \
-    --manifest spec/test_manifests/p-redis-4644ea20704f8ff3556d.yml
+    --manifest spec/test_manifests/p-redis-4644ea20704f8ff3556d.yml \
+	--stemcell 5555
 
 diff -bwy redis-photon.yml spec/test_manifests/redis-photon.yml --suppress-common-lines
