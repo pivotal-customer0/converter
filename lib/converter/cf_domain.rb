e64552cd4a1c7969bc6fb6f08f86d06aaa3922e4
Luciano Silva <lmpsilva@Lucianos-MacBook-Pro.local>
2016-02-04 23:18:06 -0500

    Fix get domain function error

--- lib/converter/cf_domain.rb
+++ lib/converter/cf_domain.rb
@@ -6,8 +6,12 @@ class CFDomain
       return cc['properties']['domain']
     end
     YAMLHelper.find yaml, 'api_url' do |api_url|
-      return api_url['api_url'].gsub 'https://api.', ''
+      unless api_url['api_url'].nil?
+         return api_url['api_url'].gsub 'https://api.', ''
+      end
     end
+    return get_job_domain yaml
+    
   end
 
   def self.change_domain(yaml, new_domain)
@@ -22,4 +26,16 @@ class CFDomain
     end
     return nil
   end
+
+  def self.get_job_domain(yaml)
+    yaml['jobs'].each do | job |
+      if job.include? 'properties' 
+        if job['properties'].include? 'domain'
+           return job['properties']['domain']
+        end      
+      end
+    end
+    return nil
+  end
+  
 end
